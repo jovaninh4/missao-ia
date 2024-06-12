2e2ee2fcc2968b1a055918b27c1cf3bd008e83a2
geovana :) <110537278+jovaninh4@users.noreply.github.com>
2024-06-12 08:20:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-const caixaPrincipal = 
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const pergunta1 = [
+    {
+        enunciado: "1. Qual é o nome do brinquedo cowboy em Toy Story?",
+        alternativas: [
+            "Alternativas 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativas 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativas 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativas 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativas 1",
+            "Alternativa 2"
+        ]
+    },
+];
